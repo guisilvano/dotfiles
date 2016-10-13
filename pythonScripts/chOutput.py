d9ee38798ed31828db5ff6b8ed7928566bb328a5
guisilvano <guisilvano@hotmail.com>
2016-10-12 22:59:42 -0300

    change subprocess.call() to os.system()

--- pythonScripts/chOutput.py
+++ pythonScripts/chOutput.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python
 
-from subprocess import call
+import sys, os
 
-print "Select output:"; print "1. LVDS1"; print "2. VGA1"; print "0. Displays"
-opt = input (">")
+while True: 
+    print "Select option:"; print "1. LVDS1"; print "2. VGA1"; print "0. Displays"
+    opt = raw_input(">")
+    
+    if opt == '1':
+        print "  LVDS1:"; print "  1. Enable"; print "  0. Disable"
+        opt = raw_input ("  >")
 
-if opt == 1:
-    print "  LVDS1:"; print "  1. Enable"; print "  0. Disable"
-    opt = input ("  >")
-
-    if opt == 1:
-        print "Calling xrandr... "
-        call(["xrandr"+" --output LVDS1 --auto"], shell=True)
-    elif opt == 0:
-        print "    Sure? (Y/n)"
-        opt = raw_input("    >")
-            
-        if opt == 'Y':
+        if opt == '1':
             print "Calling xrandr... "
-            call(["xrandr"+" --output LVDS1 --off"], shell=True)
-        else:
-            print "No changes made."
+            os.system("xrandr --output LVDS1 --auto"); print "Done."
+            
+        elif opt == '0':
+            print "    Sure? (Y/n)"
+            opt = raw_input("    >")
+            
+            if opt == 'Y':
+                print "Calling xrandr... "
+                os.system("xrandr --output LVDS1 --off"); print "Done."
+            else:
+                print "No changes made."
         
-elif opt == 2:
-    print "  VGA1:"; print "  1. Enable"; print "  0. Disable"
-    opt = input ("  >")
+    elif opt == '2':
+        print "  VGA1:"; print "  1. Enable"; print "  0. Disable"
+        opt = raw_input ("  >")
 
-    if opt == 1:
-        print "Calling xrandr... "
-        call(["xrandr"+" --output VGA1 --auto"], shell=True)
-    elif opt == 0:
-        print "Calling xrandr... "
-        call(["xrandr"+" --output VGA1 --off"], shell=True)
+        if opt == '1':
+            print "Calling xrandr... "
+            os.system("xrandr --output VGA1 --auto"); print "Done."
+        elif opt == '0':
+            print "Calling xrandr... "
+            os.system("xrandr --output VGA1 --off"); print "Done."
 
-elif opt == 0:
-    call(["xrandr"])
-
-print "Done."
+    elif opt == '0':
+        os.system("xrandr")
+    
+    else:
+        sys.exit(0)
